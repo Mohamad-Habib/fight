094addf33f820d4a71cd0ed895cc4fdcc8d77a24
mohamad-habib <habib.lakkis@gmail.com>
2023-07-26 20:37:35 -0700

    add for loop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,16 @@
-let loginBtn = document.getElementById("login")
-let usernameEl = document.getElementById("username")
-let passwordEl = document.getElementById("password")
+const accounts = [["admin01","123"],["admin02","234"],["admin03","345"]]
+const loginBtn = document.getElementById("login")
+const usernameEl = document.getElementById("username")
+const passwordEl = document.getElementById("password")
 document.title = "LogIn Page"
 
 loginBtn.addEventListener("click", function(){
     let accessUser = usernameEl.value
     let accessPass = passwordEl.value
-    console.log(accessUser)
+    for (let i=0; i<=accounts.length; i++){
+        if(accessUser == accounts[i][0]&& accessPass == accounts[i][1] ){
+            console.log(`welcome ${accessUser}`)
+        }
+    }
+
 })
