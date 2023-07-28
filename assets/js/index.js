3f371ce268e4edf9415dbe896a5e43086a6b310a
mohamad-habib <habib.lakkis@gmail.com>
2023-07-27 20:32:46 -0700

    add counter, add seconde page location

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,8 +3,10 @@ const loginBtn = document.getElementById("login")
 const usernameEl = document.getElementById("username")
 const passwordEl = document.getElementById("password")
 document.title = "LogIn Page"
+let counter = 1
 
 loginBtn.addEventListener("click", function(){
+if (counter < 4){
     let accessUser = usernameEl.value
     let accessPass = passwordEl.value
     let found = false
@@ -12,11 +14,22 @@ loginBtn.addEventListener("click", function(){
         if(accessUser == accounts[i][0] && accessPass == accounts[i][1] ){
             alert(`welcome ${accessUser}`)
             found = true
-            document.write(`<h1 style="text-align:center; color:blue; margin:50px">hello<span style="font-weight:bold; color:red"> ${accessUser.toUpperCase()}</span></h1>`)
+            counter = 1
+            document.write(`<h1 style="text-align:center; color:blue; margin:50px">hello<span style="font-weight:bold; color:red"> ${accessUser.toUpperCase(
+            )}</span></h1>`)
+            setTimeout(secondePage,2000);
+            
         } 
     }
     if (found == false){
-        alert(`incorrect username or password`)
+        counter++
+        alert(`incorrect username or password \n you still have ${4-counter} attempt`)
         
     }
+}else{
+    alert('max attempts exceeded')
+}
 })
+function secondePage(){
+    window.location.replace("login.html")
+}
